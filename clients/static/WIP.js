7bf3502b70d72e5d728cb7a8ed9ade4c9b2bd6c3
Mike <petersonmichael@Petersons-MacBook-Pro.local>
2019-11-20 11:37:17 -0800

    updated github master branch

--- clients/static/WIP.js
+++ clients/static/WIP.js
@@ -10,7 +10,25 @@
 
 
 //trumpman can move automatically once button is pressed but while loop calculates faster than setTimeout trumpman.x--
-                            while(world[trumpman.y][trumpman.x - 1] != 1 && world[trumpman.y][trumpman.x - 1] != 8)
-              setTimeout(() => trumpman.x--,300); console.log(trumpman.x);
+              while(world[trumpman.y][trumpman.x - 1] != 1 && world[trumpman.y][trumpman.x - 1] != 8)
+              setTimeout(() => trumpman.x--,300); 
+              console.log(trumpman.x);
               drawTrumpman();
               mapLevel();
+
+
+
+              ///trumpman faces the direction you pressed
+
+
+      function drawTrumpman(){
+
+        if(direction == 'trumpman'){
+          document.getElementById(direction).style.left = trumpman.x *  4 + 'vw'
+          document.getElementById(direction).style.top = trumpman.y * 5 + 'vh'}
+        else {
+          document.getElementById("trumpman").setAttribute('id',direction)
+          console.log(document.getElementById("trumpmanR"))
+          document.getElementById(direction).style.left = trumpman.x *  4 + 'vw'
+          document.getElementById("trumpman").style.top = trumpman.y *  5 + 'vh'
+        }}
