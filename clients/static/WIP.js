e86e5a08dc633f3cbc5ecf5f1d83c169f298e9a1
Mike <petersonmichael@Petersons-MacBook-Pro.local>
2019-11-26 22:11:29 -0800

    Coins and snowflakes reload after being eaten, death sound occurs at right time on subsequent deaths, alert stops after all coins grabbed

--- clients/static/WIP.js
+++ clients/static/WIP.js
@@ -16,7 +16,13 @@
               drawTrumpman();
               mapLevel();
 
+              //event listener
+              window.addEventListener("online",onFunction);
 
+
+//setInterval
+function onFunction() {setInterval((roamHilly) => hillary.y--,1500);
+}
 
               ///trumpman faces the direction you pressed
 
@@ -32,3 +38,90 @@
           document.getElementById(direction).style.left = trumpman.x *  4 + 'vw'
           document.getElementById("trumpman").style.top = trumpman.y *  5 + 'vh'
         }}
+
+
+//Investigate what this does
+        parseInt(sprite.style.left) + hillary.x + 'vw';
+
+
+
+        //old Hilly move
+        async function spriteHilly() {
+          drawHillary();
+          for(var i = 0; i< 100; i++){
+            if(hillary.direction == "down") {
+              await sleep(200);
+              hillary.y++;
+              drawHillary();
+              if(i%20 == 5){hillary.direction = 'right'};
+              console.log(hillary.direction, hillary.y)
+            }
+            if(hillary.direction == 'right'){
+              await sleep(200);
+                hillary.x++;
+                drawHillary();
+              if(i%20==8){hillary.direction = 'up'};
+            }
+            if(hillary.direction == 'up'){
+              await sleep(200);
+                hillary.y--;
+                drawHillary();
+              if(i%20==13){hillary.direction = 'left'};
+            }
+            if(hillary.direction == 'left'){
+              await sleep(200);
+                hillary.x--;
+                drawHillary();
+              if(i%20==16){hillary.direction = 'down'};
+            }
+          }
+        }
+
+
+        //a type of working loop around for bottom left corner of main trump man page
+        async function spriteArnie() {
+          while(world[arnold.y][arnold.x] != world[trumpman.y][trumpman.x]){
+          while(arnold.direction == "down" && j < 6){
+            drawArnold();
+              await sleep(300);
+              arnold.y++;
+              drawArnold();
+              j++;
+              if(j == 6){
+                arnold.direction = "right"; 
+                j = 0;
+              }
+          }
+          while(arnold.direction == "right" && j < 4){
+              // console.log(arnold.direction, arnold.x)
+              await sleep(300);
+                arnold.x++;
+                drawArnold();
+                j++
+              if(j == 3){arnold.direction = 'up'; j = 0};
+            }
+          while(arnold.direction == 'up' && j < 7){
+            // console.log(arnold.direction, arnold.x)
+              await sleep(300);
+                arnold.y--;
+                drawArnold();
+                j++;
+              if(j == 6){arnold.direction = 'left'; j = 0};
+            }
+            while(arnold.direction == 'left' && j < 3){
+              await sleep(300);
+                arnold.x--;
+                drawArnold();
+                j++;
+              if(j==3){arnold.direction = 'down'; j = 0};
+            }
+          }
+        }     
+
+
+
+        //Ghost AI Beacons
+        var hBeacon = { x:0, y:0}; 
+        var beBeacon = { x:0, y:20}; 
+        var baBeacon = { x:20, y:0}; 
+        var ABeacon = { x:25, y:0};//beacons for ghosts in scatter mode
--- public/src/app/app.component.ts
+++ public/src/app/app.component.ts
@@ -1,10 +1,62 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit{
   title = 'public';
+
+  world = [
+    [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
+    [1,3,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,3,1],
+    [1,2,8,8,8,2,8,8,8,2,8,0,8,2,8,0,0,0,8,2,8,8,8,2,1],
+    [1,2,0,8,0,2,8,6,8,2,8,0,8,2,8,8,0,8,8,2,8,6,8,2,1],
+    [1,2,0,8,0,2,8,8,8,2,8,0,8,2,8,0,8,0,8,2,8,8,8,2,1],
+    [1,2,0,8,0,2,8,8,0,2,8,0,8,2,8,0,3,0,8,2,8,2,2,2,1],
+    [1,2,0,8,0,2,8,3,8,2,8,8,8,2,8,0,0,0,8,2,8,2,2,2,1],
+    [1,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,1],
+    [1,1,1,1,1,2,1,1,1,2,1,4,5,4,1,2,1,1,1,2,1,1,1,1,1],
+    [0,0,0,0,0,2,2,2,2,2,1,0,0,0,1,2,2,2,2,2,0,0,0,0,0],
+    [1,1,1,1,1,2,1,1,1,2,1,1,1,1,1,2,1,1,1,2,1,1,1,1,1],
+    [1,3,2,2,2,2,2,2,2,2,2,2,0,2,2,2,2,2,2,2,2,2,2,3,1],
+    [1,2,1,1,2,8,0,0,0,8,2,2,8,2,2,8,0,0,0,8,2,1,1,2,1],
+    [1,2,1,1,2,8,8,0,8,8,2,8,6,8,2,8,8,0,0,8,2,1,1,2,1],
+    [1,2,1,1,2,8,0,8,0,8,2,8,8,8,2,8,0,8,0,8,2,1,1,2,1],
+    [1,2,1,1,2,8,0,0,0,8,2,8,3,8,2,8,0,0,8,8,2,1,1,2,1],
+    [1,2,1,1,2,8,0,0,0,8,2,8,2,8,2,8,0,0,0,8,2,1,1,2,1],
+    [1,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,1],
+    [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
+  ];
+
+  worldDict = {//map graphics
+    0: 'empty',
+    1: 'brick',
+    2: 'snowflake',
+    3: 'coin',
+    4: 'ghostLid',
+    5: 'ghostGate',
+    6: 'burningmoney',
+    7: 'scorebox',
+    8: 'letters',
+    9: 'score'};
+
+  ngOnInit(){
+    this.mapLevel()
+  }
+
+
+  mapLevel(){//maps the game using the array and 'graphics' from the dictionary
+    var output = '';
+    for (var i = 0; i< this.world.length; i++){
+      output += "<div class = 'row'>";
+      for(var j= 0; j< this.world[i].length; j++){
+        output += "<div class = '" + this.worldDict[this.world[i][j]] + "'></div>";}
+        output += "</div>";// console.log(output);
+      };
+    document.getElementById('world').innerHTML = output;
+  }
+
 }
